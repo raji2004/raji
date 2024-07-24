991a391feaf21ddfb0c85939b1ee1230232ddd5a
raji <kingraj1344@gmail.com>
2024-07-24 12:42:45 +0100

    feat: Add Employee entity and repository
    This commit adds the Employee entity and repository classes to the codebase. The Employee entity represents an employee with properties such as name and role. The EmployeeRepository interface extends JpaRepository to provide CRUD operations for the Employee entity. These changes lay the foundation for managing employee data in the application.

---
+++ src/test/java/com/example/springlesson2/raji/RajiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springlesson2.raji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RajiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
